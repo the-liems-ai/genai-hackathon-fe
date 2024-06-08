21c1052a9fdb5d4a097e5255058627fe62ffe359
ngocmaii00 <mailyngoc2301@gmail.com>
2024-06-09 00:48:11 +0700

    feat: landing page

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,5 +1,22 @@
-const HomePage = () => {
-    return <div>HomePage</div>
-}
+"use client";
+import React from "react";
+import { Pin } from "../components/ui/3d-pin";
 
-export default HomePage
+const HomePage = () => {
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-black ">
+      <Pin>
+        <div className="flex basis-full flex-col mt-24 items-center justify-center tracking-tight text-slate-100/50 sm:basis-1/2 w-[70rem] h-[40rem]">
+          <div className="w-[650px]">
+            <img src="./public/logo.png" alt="" />
+          </div>
+          <div className="text-slate-500 text-xl font-medium uppercase flex items-center text-center justify-center">
+            The Liems
+          </div>
+        </div>
+        <div>HomePage</div>
+      </Pin>
+    </div>
+  );
+};
+export default HomePage;
