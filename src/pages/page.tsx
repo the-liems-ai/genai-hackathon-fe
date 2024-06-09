af0850f68b19fab9c3e8061e25f19f2b4ba4c7e5
ngocmaii00 <mailyngoc2301@gmail.com>
2024-06-09 16:11:55 +0700

    feat: landing page

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -4,10 +4,10 @@ import { Pin } from "../components/ui/3d-pin";
 const HomePage = () => {
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center bg-black ">
-      <Pin className="bg-[url('./bg.png')] bg-cover bg-center">
+      <Pin className="bg-[url('/bg.png')] bg-cover bg-center">
         <div className="flex basis-full flex-col mt-8 items-center justify-center tracking-tight text-slate-100/50 sm:basis-1/2 w-[60rem] h-[35rem]">
           <div className="w-[550px]">
-            <img src="./logo.png" alt="logo" />
+            <img src="/logo.png" alt="logo" />
           </div>
           <div className="text-slate-500 mt-10 text-lg font-normal uppercase flex items-center text-center justify-center">
             The Liems
